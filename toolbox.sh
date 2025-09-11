bdd99e48059e8ade571425ae5180df7e27532881
somogibbs <somogibbs@gmail.com>
2025-09-11 16:40:53 -0500

    Update toolbox.sh
    cleaned up package list

--- toolbox.sh
+++ toolbox.sh
@@ -10,31 +10,12 @@ apt update
 
 echo "Installing XFCE system packages..."
 apt install -y \
-  xfce4 \
-  xfce4-goodies \
-  xarchiver \
-  thunar-archive-plugin \
-  xfce4-power-manager \
-  package-update-indicator \
-  blueman
+  xfce4 xfce4-goodies xarchiver thunar-archive-plugin \ 
+  xfce4-power-manager package-update-indicator blueman
 
 echo "Installing toolbox packages..."
 apt install -y \
-  hardinfo \
-  mpv \
-  gtkhash \
-  guymager \
-  wxhexeditor \
-  clonezilla \
-  testdisk \
-  pdfarranger \
-  zenmap \
-  wireshark \
-  isomaster \
-  qdiskinfo \
-  kdiskmark \
-  bleachbit \
-  gparted \
-  gsmartcontrol
+  hardinfo gtkhash guymager wxhexeditor clonezilla testdisk bleachbit \ 
+  pdfarranger zenmap wireshark qdiskinfo kdiskmark gparted gsmartcontrol
 
 echo "Installation complete..."
